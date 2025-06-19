ee7c8aed0546082af4d00f9ab407d612a66d2114
ghost-mann <iamaob8@protonmail.com>
2025-06-19 10:25:50 +0300

    feat: storing csv values in dataframes in main.py

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ import pandas as pd
 # Download 5 years of XAUUSD prices
 xau = yf.download("GLD", start="2019-01-01", end="2025-06-18")
 xau.to_csv("gld_gold_proxy.csv")
+
+# store csv values in data frame
+df = pd.read_csv("gld_gold_proxy.csv")
+df = df.rename(columns={"Date":"ds", "Close":"y"})
+df["ds"] = pd.to_datetime(df["ds"])
+
+model = Prophet()
+model.fit(df)
+
